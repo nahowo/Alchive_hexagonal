8081b5379a9cc022ca733ea364a79746532d0a08
nahowo <nahowo@naver.com>
2025-01-30 01:07:35 +0900

    user create 단위테스트 작성

---
+++ src/test/java/com/Alchive/backend/service/UserTest.java
@@ -0,0 +1,19 @@
+package com.Alchive.backend.service;
+
+import com.Alchive.backend.model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+    @DisplayName("사용자 생성 - 성공")
+    @Test
+    public void userCreateTest() {
+        User sut = new User();
+
+        sut.createUser("test@gmail.com", "test");
+
+        Assertions.assertEquals("test@gmail.com", sut.getEmail());
+        Assertions.assertEquals("test", sut.getName());
+    }
+}
