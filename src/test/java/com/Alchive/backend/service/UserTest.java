34cda060b367c4a680c63fc222e37a9137656fe0
nahowo <nahowo@naver.com>
2025-02-06 21:22:55 +0900

    사용자 생성 실패 테스트 추가

--- src/test/java/com/Alchive/backend/service/UserTest.java
+++ src/test/java/com/Alchive/backend/service/UserTest.java
@@ -1,5 +1,7 @@
 package com.Alchive.backend.service;
 
+import com.Alchive.backend.config.error.exception.user.UserEmailExistException;
+import com.Alchive.backend.config.error.exception.user.UserNameExistException;
 import com.Alchive.backend.model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -8,12 +10,28 @@ import org.junit.jupiter.api.Test;
 public class UserTest {
     @DisplayName("사용자 생성 - 성공")
     @Test
-    public void userCreateTest() {
+    public void userCreateTestSuccess() {
         User sut = new User();
 
         sut.createUser("test@gmail.com", "test", false, false);
 
         Assertions.assertEquals("test@gmail.com", sut.getEmail());
         Assertions.assertEquals("test", sut.getName());
+    }
+
+    @DisplayName("사용자 생성 - 메일 중복 실패")
+    @Test
+    public void userCreateTestFailByEmailOverlap() {
+        User sut = new User();
+
+        Assertions.assertThrows(UserEmailExistException.class, () -> sut.createUser("test@gmail.com", "test", true, false));
+    }
+
+    @DisplayName("사용자 생성 - 사용자명 중복 실패")
+    @Test
+    public void userCreateTestFailByNameOverlap() {
+        User sut = new User();
+
+        Assertions.assertThrows(UserNameExistException.class, () -> sut.createUser("test@gmail.com", "test", false, true));
     }
 }
